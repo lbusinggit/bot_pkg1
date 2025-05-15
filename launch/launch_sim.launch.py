41e288e73cea47f18c4a7b396d6b95c71ec1e897
Lalith Bhaskaran <lalithbhaskaran16@gmail.com>
2025-05-15 20:22:52 +0530

    Config changes and Robot Launch File

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -13,19 +13,37 @@ def generate_launch_description():
             get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-    )
+        launch_arguments={'extra_gazebo_args': '--ros-args --params-file '+ gazebo_params_file}.items()
 
+    )   
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package='controller_manager',
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
